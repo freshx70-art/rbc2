db020b0dc08e4200f2ea5f198c6ce403539c50c6
freshx70-art <freshx70@gmail.com>
2026-05-26 11:03:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
