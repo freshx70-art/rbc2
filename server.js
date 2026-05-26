d9c569e2f70e95cb58e4eb2be674474af69e8e9a
freshx70-art <freshx70@gmail.com>
2026-05-26 11:33:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const cors = require('cors');
+const cors = require('cors');  // Add this line
 const app = express();
 const port = 3001;  // Ensure this line is correct
 
